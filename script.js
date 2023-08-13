3595f8e626c280e98cc1072cacd3301bfca013ee
Kevin Kinner <kevin-kinner@web.de>
2023-08-13 20:30:15 +0200

    Done

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
+console.log("Script running");
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
 }
 
+function addBook() {
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let pages = document.getElementById("pages").value;
+	let read = document.getElementById("r1y").checked;
 
-document.getElementById("confirm").addEventListener("click", ()=> {
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let pages = document.getElementById('pages').value;
-    let read = read()
-    let book = new Book(title, author, pages, read);
-    console.log(book);
-})
-
-function read() {
-    if (document.getElementById('r1y').checked) {
-        return true;
-    } else {
-        return false;
-    }
+	let newBook = new Book(title, author, pages, read);
+	let newDiv = document.createElement("div");
+	newDiv.textContent =
+		newBook.title +
+		" by " +
+		newBook.author +
+		" with a " +
+		newBook.pages +
+		" pages  " +
+		newBook.read;
+	document.getElementById("library").appendChild(newDiv);
 }
